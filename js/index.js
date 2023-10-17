9be9102b91c0287c3c5c4a4ba8a97e4c5f70e81f
nat-paz <nataliapazsa@gmail.com>
2023-10-16 21:42:48 -0300

    up

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import faq from "./modules/faq.js"
 import conteudo from "./modules/conteudo.js"
 import menuMobileInit from "./modules/menu.js"
+import modal from "./modules/modal.js"
 
 faq()
 conteudo()
 menuMobileInit()
+modal()
